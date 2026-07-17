733942b4eeef6a3cb3eb36ef69b6b2161fdb658a
btseng <btseng1970@gmail.com>
2026-07-17 22:14:03 +0800

    feat(fnd-001): bootstrap application workspaces

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/coverage/**',
+    ],
+  },
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+);
